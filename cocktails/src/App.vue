542104ae7cefface4dd47c4d7c84fcbfa827130d
Ivelin Dzhevelekov <ivelin.dzhevelekov@gmail.com>
2023-06-28 15:55:45 +0300

    Add simple routing

--- cocktails/src/App.vue
+++ cocktails/src/App.vue
@@ -1,32 +1,23 @@
 <script setup>
-import { ref, onMounted } from 'vue'
-const cocktails = ref(null)
-onMounted(async() => {
-  const apiUrl = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Ordinary_Drink'
-  const response = await fetch(apiUrl) 
-  cocktails.value = await response.json()
-  console.log(cocktails.value.drinks)
-  cocktails.value = cocktails.value.drinks.map(drink => {
-    return {
-      name:drink.strDrink,
-      image:drink.strDrinkThumb
-    }
-  } );
+import { ref, computed } from 'vue'
+import CocktailsList from './components/CocktailsList.vue';
+import HelloWorld from './components/HelloWorld.vue';
+
+const routes = {
+  '/': CocktailsList
+}
+
+const currentPath = ref(window.location.hash)
+
+window.addEventListener('hashchange', () => {
+  currentPath.value = window.location.hash
+})
+
+const currentView = computed(() => {
+  return routes[currentPath.value.slice(1) || '/'] || HelloWorld
 })
 </script>
 
 <template>
-  <h1 class = "centre" >Cocktails</h1>
-  <div class = "row" v-if="cocktails">
-      <div v-for="cocktail in cocktails" class="card col-md-4 m-2" style="width: 18rem;">
-          <a href=""><img class="card-img-top" :src=cocktail.image alt="Card image cap"></a>
-          <div class="card-body">
-          <h5 class="card-title">{{cocktail.name}}</h5>
-          </div>
-        </div>
-  </div>
+  <component :is="currentView" />
 </template>
-
-<style>
-
-</style>
---
+++ cocktails/src/components/CocktailDetails.vue
@@ -0,0 +1,15 @@
+<script>
+    import { ref, onMounted } from 'vue'
+    const cocktailId = "13581"
+    const url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" + cocktailId
+    onMounted(async() => {
+        const response = await fetch(url)
+        console.log(response)
+    })
+</script>
+
+
+
+<template>
+
+</template>
